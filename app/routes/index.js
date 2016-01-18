e7e09703a8f702e1e17619a75901f7304f538a40
Stefan Penner <stefan.penner@gmail.com>
2016-01-18 00:20:38 -0600

    extract example world

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import { world, alive } from 'conways/lib/world';
+import world from 'conways/lib/worlds/one';
 
 export default Ember.Route.extend({
   setupController(controller) {
@@ -15,6 +15,6 @@ export default Ember.Route.extend({
   },
 
   model() {
-    return world(99, 99, ...alive(99, 99, [1,49], [2,49], [3,49], [4,49], [5,49], [6,49], [7,49], [8,49], [10,49], [11,49], [12,49], [13,49], [14,49], [18,49], [19,49], [20,49], [27,49], [28,49], [29,49], [30,49], [31,49], [32,49], [33,49], [35,49], [36,49], [37,49], [38,49], [39,49]));
+    return world;
   }
 });
